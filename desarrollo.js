7666a559cfbad7cb1df623fc6afb9b17c30a319d
FrankElProgramador <53879356+FrankElProgramador@users.noreply.github.com>
2025-04-29 17:02:37 -0500

    Cambios
    cambios

---
+++ desarrollo.js
@@ -0,0 +1,83 @@
+let botonEnviar = document.getElementById("Abrir1");
+let caja = document.getElementById("Caja");
+let caja2 = document.getElementById("Caja2");
+let caja3 = document.getElementById("Caja3");
+let caja4 = document.getElementById("Caja4");
+let caja5 = document.getElementById("Caja5");
+let caja6 = document.getElementById("Caja6");
+let caja7 = document.getElementById("Caja7");
+let caja8 = document.getElementById("Caja8");
+caja.style.display = "none";
+caja2.style.display = "none";
+caja3.style.display = "none";
+caja4.style.display = "none";
+caja5.style.display = "none";
+caja6.style.display = "none";
+caja7.style.display = "none";
+caja8.style.display = "none";
+
+function Abriruno()
+{
+    if (caja.style.display === "none") {
+        caja.style.display = 'block';
+} else {
+    caja.style.display = "none";
+}
+}
+
+function AbrirDos()
+{
+    if (caja2.style.display === "none") {
+        caja2.style.display = 'block';
+} else {
+    caja2.style.display = "none";
+}
+}
+function AbrirTres()
+{
+    if (caja3.style.display === "none") {
+        caja3.style.display = 'block';
+} else {
+    caja3.style.display = "none";
+}
+}
+function AbrirCuatro()
+{
+    if (caja4.style.display === "none") {
+        caja4.style.display = 'block';
+} else {
+    caja4.style.display = "none";
+}
+}
+function AbrirCinco()
+{
+    if (caja5.style.display === "none") {
+        caja5.style.display = 'block';
+} else {
+    caja5.style.display = "none";
+}
+}
+function AbrirSeis()
+{
+    if (caja6.style.display === "none") {
+        caja6.style.display = 'block';
+} else {
+    caja6.style.display = "none";
+}
+}
+function AbrirSiete()
+{
+    if (caja7.style.display === "none") {
+        caja7.style.display = 'block';
+} else {
+    caja7.style.display = "none";
+}
+}
+function AbrirOcho()
+{
+    if (caja8.style.display === "none") {
+        caja8.style.display = 'block';
+} else {
+    caja8.style.display = "none";
+}
+}
